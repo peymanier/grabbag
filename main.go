009bbbe8ebf99d9a539c0a487b25f4f92b63ceae
Peyman <hassani.peyman627@gmail.com>
2025-11-21 11:02:21 +0330

    Add chi to main.go

--- main.go
+++ main.go
@@ -2,32 +2,15 @@ package main
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
-	"math/big"
-	"strconv"
-
-	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/pgtype"
-	"github.com/joho/godotenv"
-	"github.com/peymanier/grabbag/database"
-
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
 )
-
-type NobitexTrade struct {
-	Time   int    `json:"time"`
-	Price  string `json:"price"`
-	Volume string `json:"volume"`
-	Type   string `json:"type"`
-}
-
-type NobitexResponse struct {
-	Status string         `json:"status"`
-	Trades []NobitexTrade `json:"trades"`
-}
 
 func main() {
 	err := godotenv.Load()
@@ -42,48 +25,15 @@ func main() {
 	}
 	defer conn.Close(ctx)
 
-	queries := database.New(conn)
+	//queries := database.New(conn)
 
-	resp, err := http.Get("https://apiv2.nobitex.ir/v2/trades/USDTIRT")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Heartbeat("/ping"))
+
+	err = http.ListenAndServe(":3333", r)
 	if err != nil {
-		log.Println(err)
-		return
+		log.Fatal(err)
 	}
-	defer resp.Body.Close()
-
-	response := NobitexResponse{}
-	err = json.NewDecoder(resp.Body).Decode(&response)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	price, err := strconv.Atoi(response.Trades[0].Price)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	priceBigInt := big.NewInt(int64(price))
-	asset, err := queries.CreateAsset(context.Background(), database.CreateAssetParams{
-		Code:  "USDT",
-		Name:  "USD Tether",
-		Price: pgtype.Numeric{Int: priceBigInt, Valid: true},
-	})
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	assetPriceLog, err := queries.CreateAssetPriceLog(context.Background(), database.CreateAssetPriceLogParams{
-		AssetID: pgtype.Int8{Int64: asset.ID, Valid: true},
-		Price:   pgtype.Numeric{Int: priceBigInt, Valid: true},
-	})
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	fmt.Println(asset)
-	fmt.Println(assetPriceLog)
 }
