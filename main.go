a5b9476daa83c41195a50c47d7e7d3b05b17ace5
Peyman <hassani.peyman627@gmail.com>
2025-11-22 13:04:24 +0330

    Add `RunMigrations` function

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -9,8 +10,10 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/stdlib"
 	"github.com/joho/godotenv"
 	"github.com/peymanier/grabbag/database"
+	"github.com/pressly/goose/v3"
 )
 
 type Server struct {
@@ -33,6 +36,43 @@ func (s *Server) MountHandlers() {
 	s.Router.Get("/assets", s.ListAssets)
 }
 
+func RunMigrations(ctx context.Context, connStr, migrationsDir string) error {
+	if err := goose.SetDialect("postgres"); err != nil {
+		return fmt.Errorf("failed to set dialect: %s", err)
+	}
+
+	config, err := pgx.ParseConfig(connStr)
+	if err != nil {
+		return err
+	}
+	db := stdlib.OpenDB(*config)
+	defer db.Close()
+
+	currVersion, err := goose.GetDBVersionContext(ctx, db)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("current migration version: %d", currVersion)
+
+	if err := goose.UpContext(ctx, db, migrationsDir); err != nil {
+		return fmt.Errorf("failed to apply migrations: %s", err)
+	}
+
+	newVersion, err := goose.GetDBVersionContext(ctx, db)
+	if err != nil {
+		return err
+	}
+
+	if newVersion > currVersion {
+		log.Printf("succesfully migrated from version %d to %d", currVersion, newVersion)
+	} else {
+		log.Printf("database is already up to date at version %d", currVersion)
+	}
+
+	return nil
+}
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
@@ -40,11 +80,18 @@ func main() {
 	}
 
 	ctx := context.Background()
-	conn, err := pgx.Connect(ctx, os.Getenv("POSTGRES_URL"))
+	connStr := os.Getenv("POSTGRES_URL")
+
+	conn, err := pgx.Connect(ctx, connStr)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer conn.Close(ctx)
+
+	err = RunMigrations(ctx, connStr, "./sql/schema")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	s := NewServer(conn)
 	s.MountHandlers()
