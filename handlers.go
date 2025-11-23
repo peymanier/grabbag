b61b13caed23926f599310408cc2694de4c2ceeb
Peyman <hassani.peyman627@gmail.com>
2025-11-23 13:28:14 +0330

    Update ListAsset handler to use html templates instead of json

--- handlers.go
+++ handlers.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"net/http"
-
-	"github.com/peymanier/grabbag/messages"
+	"text/template"
 )
 
 func (s *Server) ListAssets(w http.ResponseWriter, r *http.Request) {
 	assets, err := s.Queries.ListAssets(r.Context())
 	if err != nil {
-		JSONResponse(w, http.StatusInternalServerError, messages.ErrUnknown)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
-	JSONResponse(w, http.StatusOK, AssetsToResponse(assets))
+	tmpl := template.Must(template.ParseFiles("templates/assets.gohtml"))
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	err = tmpl.Execute(w, assets)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
