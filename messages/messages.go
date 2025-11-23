d3dd609e25a117a02ce1482e394d4bcb2e199884
Peyman <hassani.peyman627@gmail.com>
2025-11-23 13:27:55 +0330

    Add String method on messages

--- messages/messages.go
+++ messages/messages.go
@@ -1,8 +1,14 @@
 package messages
 
+import "fmt"
+
 type Message struct {
 	Code   int    `json:"code"`
 	Detail string `json:"detail"`
+}
+
+func (m Message) String() string {
+	return fmt.Sprintf("detail: %s, code: %d", m.Detail, m.Code)
 }
 
 var (
