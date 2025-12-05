94df0ae23aa0cf7e987572c2f1b8168afc274495
Peyman <hassani.peyman627@gmail.com>
2025-12-05 17:47:53 +0330

    Update dto objects

--- dto.go
+++ dto.go
@@ -7,26 +7,32 @@ import (
 	"github.com/peymanier/grabbag/pgconv"
 )
 
-type AssetDTO struct {
+type AssetsWithPriceChangesDTO struct {
 	ID        int64
 	Code      string
 	Price     string
 	CreatedAt time.Time
 	UpdatedAt time.Time
+	Change4H  string
+	Change1D  string
+	Change7D  string
 }
 
-func AssetToDTO(asset database.Asset) AssetDTO {
-	return AssetDTO{
+func AssetToDTO(asset database.ListAssetsWithPriceChangesRow) AssetsWithPriceChangesDTO {
+	return AssetsWithPriceChangesDTO{
 		ID:        asset.ID,
 		Code:      asset.Code,
 		Price:     *pgconv.NumericToString(asset.Price),
 		CreatedAt: *pgconv.TimestamptzToTime(asset.CreatedAt),
 		UpdatedAt: *pgconv.TimestamptzToTime(asset.UpdatedAt),
+		Change4H:  *pgconv.NumericToString(asset.Change4h),
+		Change1D:  *pgconv.NumericToString(asset.Change1d),
+		Change7D:  *pgconv.NumericToString(asset.Change7d),
 	}
 }
 
-func AssetsToDTO(assets []database.Asset) []AssetDTO {
-	var res []AssetDTO
+func AssetsToDTO(assets []database.ListAssetsWithPriceChangesRow) []AssetsWithPriceChangesDTO {
+	var res []AssetsWithPriceChangesDTO
 	for _, asset := range assets {
 		res = append(res, AssetToDTO(asset))
 	}
