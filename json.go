db617cbf1d8799a5fa5ea826c446e48eba15eb1d
Peyman <hassani.peyman627@gmail.com>
2025-12-11 14:42:17 +0330

    Add json file back

---
+++ json.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func JSONResponse(w http.ResponseWriter, code int, payload any) {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(data)
+}
